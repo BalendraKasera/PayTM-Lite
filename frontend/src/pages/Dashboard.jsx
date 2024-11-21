3b3d9548459cb28229b68e6b04da8292fdb6101d
BalendraKasera <balendrkasera2024@gmail.com>
2024-11-21 17:00:43 +0530

    changes_done

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,16 @@
-const Dashboard =()=>{
-    return <div>
+import Appbar  from "../components/Appbar"
+import  Balance  from "../components/Balance"
+import Users  from "../components/Users"
 
+
+ const Dashboard = () => {
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
     </div>
 }
+
 export default Dashboard;
